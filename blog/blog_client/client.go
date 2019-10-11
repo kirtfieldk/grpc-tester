96f42293b81abcb5a0bfcf103026bb5fea369fd3
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-10-10 22:09:20 -0400

    read

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -19,15 +19,28 @@ func main() {
 	defer cc.Close()
 	c := blogpb.NewBlogServiceClient(cc)
 	// Createing blog
-	blog := &blogpb.Blog{
-		AuthorId: "Keith Kirtfield",
-		Title:    "Miser and a tree",
-		Content:  "A simple book",
-	}
-	res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
-	if err != nil {
-		log.Fatalf("Error adding blog: %v", err)
-	}
-	fmt.Printf("Added blog: %v", res)
+	// blog := &blogpb.Blog{
+	// 	AuthorId: "Keith Kirtfield",
+	// 	Title:    "Miser and a tree",
+	// 	Content:  "A simple book",
+	// }
+	// res, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	// if err != nil {
+	// 	log.Fatalf("Error adding blog: %v", err)
+	// }
+	// fmt.Printf("Added blog: %v", res)
 
+	// read Blog
+	fmt.Println("Envoke Read blog Func")
+
+	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "weifhewfhefoijewfe"})
+	if err2 != nil {
+		fmt.Printf("Error: %v", err2)
+	}
+	readBlog, err3 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "5d9fd5110b4e6da8a2f6dbef"})
+	if err3 != nil {
+		fmt.Printf("Error: %v", err3)
+	}
+
+	fmt.Printf("Blog retived: %v", readBlog)
 }
--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"github.com/keithkfield/grpc-go-course-tester/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
@@ -23,7 +25,7 @@ type server struct {
 }
 
 type item struct {
-	// ID       objectid.ObjectID
+	ID       primitive.ObjectID
 	AuthorID string `bson: "author_id"`
 	Content  string `bson: "content"`
 	Title    string `bson: "title"`
@@ -85,13 +87,44 @@ func (*server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) (*
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal Error: %v", err))
 	}
-	fmt.Printf("Added: %v", res)
+	oid, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return nil, status.Errorf(
+			codes.Internal,
+			fmt.Sprintf("Cannot convert to OID"),
+		)
+	}
 
 	return &blogpb.CreateBlogResponse{
 		Blog: &blogpb.Blog{
+			Id:       oid.Hex(),
 			AuthorId: blog.GetAuthorId(),
 			Title:    blog.GetTitle(),
 			Content:  blog.GetContent(),
+		},
+	}, nil
+}
+
+func (*server) ReadBlog(ctx context.Context, req *blogpb.ReadBlogRequest) (*blogpb.ReadBlogResponse, error) {
+	blogId := req.GetBlogId()
+
+	oid, err := primitive.ObjectIDFromHex(blogId)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Error Fetch blog: %v", err))
+	}
+	// Create empty struct
+	data := &item{}
+	filter := bson.M{"_id": oid}
+	res := collection.FindOne(context.Background(), filter)
+	if responseErr := res.Decode(data); err != nil {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Cannot find blog with specified ID: %v", responseErr))
+	}
+	return &blogpb.ReadBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:       data.ID.Hex(),
+			AuthorId: data.AuthorID,
+			Title:    data.Title,
+			Content:  data.Content,
 		},
 	}, nil
 }
